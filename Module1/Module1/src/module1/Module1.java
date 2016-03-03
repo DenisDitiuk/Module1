363b74a2b9f836ba138cf612ecd58cb3359e38b0
DenisDitiuk <dendituk@mail.ru>
2016-03-03 19:21:56 +0200

    Модуль1

---
+++ Module1/Module1/src/module1/Module1.java
@@ -0,0 +1,9 @@
+package module1;
+
+public class Module1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");  
+    }
+    
+}
